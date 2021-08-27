c2fcc26b6e2f55bc9ae1a0f752f3596e7e8a34c9
Y Tran <tranquocymail@gmail.com>
2021-08-27 14:59:04 +0700

    Fix error end of line MacOS

--- src/components/Loading/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import style from './style.css';
-
-const Loading = () => <div className={style.loading} />;
-
-export default Loading;
--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-import APIService from './APIServices';
-import WeatherService from './WeatherService';
-
-const apiServiceInstance = new APIService();
-const weatherService = new WeatherService(apiServiceInstance);
-
-export { weatherService };
